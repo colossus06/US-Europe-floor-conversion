95b168e407a4019760c6d4780ffaccfeb19d9c53
gttechada <96833570+colossus06@users.noreply.github.com>
2022-04-13 09:33:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#/bin/usr/python3
+
+#This is a simple USA-Europe floor converter
+
+print("Welcome to Europe.. Interrail is fun, isn't it...")
+eUinput=input("Enter your Europe floor: ")
+uSinput= int(eUinput) + 1
+print("You're in",uSinput, "floor in USA!" )
+
+    
+
